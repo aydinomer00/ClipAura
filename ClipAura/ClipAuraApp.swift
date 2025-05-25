eeb1342aa45d203a05881130833ecd377fd3b036
aydinomer00 <aydinomer00@gmail.com>
2025-05-25 22:06:24 +0300

    Initial Commit

---
+++ ClipAura/ClipAuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipAuraApp.swift
+//  ClipAura
+//
+//  Created by Ömer Murat Aydın on 25.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ClipAuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipAura/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClipAura
+//
+//  Created by Ömer Murat Aydın on 25.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
